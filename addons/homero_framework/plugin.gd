a9a6ded26152a81805cf0010ceca2967426f5668
R4nKF1v3 <nahumoscatelli@gmail.com>
2025-09-24 11:27:21 -0300

    chore: Minor cleanup on plugin.gd

--- addons/homero_framework/plugin.gd
+++ addons/homero_framework/plugin.gd
@@ -1,16 +1,16 @@
 @tool
 extends EditorPlugin
 
-const version_checker_scene: PackedScene = preload(
+const VERSION_CHECKER_SCENE: PackedScene = preload(
 	"res://addons/homero_framework/src/core/editor/version/version_checker.tscn"
 )
-
-const dialogue_editor_inspector_plugin_script: Script = preload(
+const DIALOGUE_EDITOR_INSPECTOR_PLUGIN_SCRIPT: Script = preload(
 	"res://addons/homero_framework/src/core/editor/dialogue/dialogue_editor_inspector_plugin.gd"
 )
-const dialogue_editor_scene: PackedScene = preload(
+const DIALOGUE_EDITOR_SCENE: PackedScene = preload(
 	"res://addons/homero_framework/src/core/editor/dialogue/dialogue_graph_editor.tscn"
 )
+const EVENTS_MANAGER_PATH: String = "res://addons/homero_framework/src/core/components/events/events_manager.gd"
 
 var version_checker: Control
 var dialogue_editor_inspector_plugin: EditorInspectorPlugin
@@ -20,12 +20,14 @@ var dialogue_editor_button: Button
 
 # Initialization of the plugin.
 func _enter_tree() -> void:
-	version_checker = version_checker_scene.instantiate()
+	version_checker = VERSION_CHECKER_SCENE.instantiate()
 	add_control_to_container(EditorPlugin.CONTAINER_CANVAS_EDITOR_BOTTOM, version_checker)
 	
-	dialogue_editor_inspector_plugin = dialogue_editor_inspector_plugin_script.new()
+	dialogue_editor_inspector_plugin = DIALOGUE_EDITOR_INSPECTOR_PLUGIN_SCRIPT.new()
 	dialogue_editor_inspector_plugin.edit_called.connect(_on_dialogue_editor_inspector_plugin_edit_called)
 	add_inspector_plugin(dialogue_editor_inspector_plugin)
+	
+	add_autoload_singleton("EventsManager", EVENTS_MANAGER_PATH)
 	
 	var plugin_version: String = get_plugin_version()
 	version_checker.check_version(plugin_version)
@@ -49,14 +51,15 @@ func _exit_tree() -> void:
 		dialogue_editor.queue_free()
 		dialogue_editor = null
 	
+	remove_autoload_singleton("EventsManager")
+	
 	HFLog.w("Unloaded successfully")
-
 
 
 func _on_dialogue_editor_inspector_plugin_edit_called(dialogue_sequence: HFDialogueSequence) -> void:
 	print("Edit dialogue sequence called for %s" % dialogue_sequence)
 	if dialogue_editor == null:
-		dialogue_editor = dialogue_editor_scene.instantiate()
+		dialogue_editor = DIALOGUE_EDITOR_SCENE.instantiate()
 		dialogue_editor_button = add_control_to_bottom_panel(dialogue_editor, "Dialogue Editor")
 	dialogue_editor.setup(dialogue_sequence)
 	dialogue_editor_button.set_pressed(true)
