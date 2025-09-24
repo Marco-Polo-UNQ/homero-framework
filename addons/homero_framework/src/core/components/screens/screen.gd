99c1caf338db78b0f7b386bd6d6503d92e968a56
R4nKF1v3 <nahumoscatelli@gmail.com>
2025-09-24 12:21:11 -0300

    chore: Added documentation to screen.gd. Refactored as @abstract class

--- addons/homero_framework/src/core/components/screens/screen.gd
+++ addons/homero_framework/src/core/components/screens/screen.gd
@@ -1,13 +1,24 @@
+@abstract
 class_name HFScreen
 extends Node
+## Base class for all screens in the Homero Framework.
+##
+## Extend this class to implement custom screen logic. Used by [HFScreenLoader] and [HFScreenManager].
+## Handles screen entry and exit, and provides signals for screen changes and exit completion.
 
-signal change_screen(target_screen: int, value)
+## Emitted when the screen requests a change to another screen.
+## [param target_screen] The id of the target screen.
+## [param value] Optional value to pass to the target screen.
+signal change_screen(target_screen: int, value: Variant)
+
+## Emitted when the screen has finished its exit logic and can be safely removed.
 signal finished_exit()
 
+## Called when the screen is entered. Override to implement custom logic.
+## [param value] Optional value passed to the screen on entry.
+@abstract
+func enter(value: Variant = null) -> void
 
-func enter(value = null) -> void:
-	pass
-
-
+## Called when the screen is exited. Override to implement custom logic.
 func exit() -> void:
 	finished_exit.emit()
