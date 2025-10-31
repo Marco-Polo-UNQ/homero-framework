73aa2cf32ccfe710a18802206244de5715e8d279
R4nKF1v3 <nahumoscatelli@gmail.com>
2025-10-31 14:45:32 -0300

    feat: Added dialogue speaker tests and code comments

--- addons/homero_framework/src/core/components/dialogues/dialogue_speaker.gd
+++ addons/homero_framework/src/core/components/dialogues/dialogue_speaker.gd
@@ -1,7 +1,17 @@
 class_name HFDialogueSpeaker
 extends Resource
+## Wrapper of [HFDialogueSpeakerResource] for Dialogue Editor handling.
 
+## The actual editable speaker data resource for this speaker.
 @export var speaker_data: HFDialogueSpeakerResource
 
-@export_category("Editor Metadata (ignore)")
-@export var graph_position: Vector2
+# Dialogue editor graph position metadata, not visible in editor inspector.
+@export_storage var graph_position: Vector2
+
+
+func _init(
+	p_speaker_data: HFDialogueSpeakerResource = null,
+	p_graph_position: Vector2 = Vector2.ZERO
+) -> void:
+	speaker_data = p_speaker_data
+	graph_position = p_graph_position
---
+++ tests/dialogue/logic/test_dialogue_speaker.gd
@@ -0,0 +1,35 @@
+extends GutTest
+
+var dialogue_speaker: HFDialogueSpeaker
+
+var speaker_data_stub: HFDialogueSpeakerResource
+
+
+func before_each() -> void:
+	speaker_data_stub = double(HFDialogueSpeakerResource).new()
+	
+	dialogue_speaker = HFDialogueSpeaker.new(
+		speaker_data_stub,
+		Vector2.ZERO
+	)
+
+
+func test_dialogue_sequence_exists() -> void:
+	assert_not_null(
+		dialogue_speaker,
+		"Dialogue Speaker should instantiate and not be null"
+	)
+
+
+func test_dialogue_sequence_has_starting_steps() -> void:
+	assert_eq(
+		dialogue_speaker.speaker_data,
+		speaker_data_stub
+	)
+
+
+func test_dialogue_sequence_has_dialogue_steps() -> void:
+	assert_eq(
+		dialogue_speaker.graph_position,
+		Vector2.ZERO
+	)
