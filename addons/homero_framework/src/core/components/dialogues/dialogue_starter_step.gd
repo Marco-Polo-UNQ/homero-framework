10191b819c21c524a900dea156c127cc307849d3
R4nKF1v3 <nahumoscatelli@gmail.com>
2025-10-31 16:24:38 -0300

    feat: Added dialogue starter step tests and code comments/docs

--- addons/homero_framework/src/core/components/dialogues/dialogue_starter_step.gd
+++ addons/homero_framework/src/core/components/dialogues/dialogue_starter_step.gd
@@ -1,21 +1,31 @@
 class_name HFDialogueStarterStep
 extends Resource
+## Dialogue starter step resource for dialogue sequences.
+##
+## Represents a starting condition in a dialogue sequence, linked to a
+## [HFDialogueStep] with optional conditional events for activation.
 
+## The id of the [HFDialogueStep] to serve as the start of the dialogue sequence
+## in a given sequence.
 @export var step_id: int
+## Conditions that must be met for this starting sequence to be enabled.
 @export var enable_conditions: Array[HFEventConditional]
 
-@export_category("Editor Metadata (ignore)")
-@export var graph_position: Vector2
+# Editor graph position metadata.
+@export_storage var graph_position: Vector2
 
-
+## Initializes the dialogue starter step with parameters.
 func _init(
 	p_step_id: int = 0,
-	p_enable_conditions: Array[HFEventConditional] = []
+	p_enable_conditions: Array[HFEventConditional] = [],
+	p_graph_position: Vector2 = Vector2.ZERO
 ) -> void:
 	step_id = p_step_id
 	enable_conditions = p_enable_conditions
+	graph_position = p_graph_position
 
-
+## Returns true if all [method HFEventConditional.can_trigger_condition] in
+## [member enable_conditions] return true.
 func is_enabled() -> bool:
 	var is_enabled: bool = true
 	for condition in enable_conditions:
---
+++ tests/dialogue/logic/test_dialogue_starter_step.gd
@@ -0,0 +1,68 @@
+extends GutTest
+
+var dialogue_starter_step: HFDialogueStarterStep
+
+var event_conditional_stub: HFEventConditional
+
+
+func before_each() -> void:
+	event_conditional_stub = double(HFEventConditional).new()
+	
+	dialogue_starter_step = HFDialogueStarterStep.new(
+		0,
+		[event_conditional_stub] as Array[HFEventConditional],
+		Vector2.ZERO
+	)
+
+
+func test_dialogue_starter_step_exists() -> void:
+	assert_not_null(
+		dialogue_starter_step,
+		"Dialogue Starter Step should instantiate and not be null"
+	)
+
+
+func test_dialogue_starter_step_has_a_step_id() -> void:
+	assert_eq(
+		dialogue_starter_step.step_id,
+		0
+	)
+
+
+func test_dialogue_starter_step_has_enable_conditions() -> void:
+	assert_eq_deep(
+		dialogue_starter_step.enable_conditions,
+		[event_conditional_stub]
+	)
+
+
+func test_dialogue_starter_step_has_a_graph_position() -> void:
+	assert_eq(
+		dialogue_starter_step.graph_position,
+		Vector2.ZERO
+	)
+
+
+func test_dialogue_starter_step_is_enabled_returns_true_if_enable_conditions_is_empty() -> void:
+	dialogue_starter_step.enable_conditions = []
+	assert_true(dialogue_starter_step.is_enabled())
+
+
+func test_dialogue_starter_step_is_enabled_returns_true_if_all_enable_conditions_are_enabled() -> void:
+	stub(event_conditional_stub.can_trigger_condition.bind("", true, EventsManager.events_map)).to_return(true)
+	assert_true(dialogue_starter_step.is_enabled())
+	
+	var another_condition_stub: HFEventConditional = double(HFEventConditional).new()
+	stub(another_condition_stub.can_trigger_condition.bind("", true, EventsManager.events_map)).to_return(true)
+	dialogue_starter_step.enable_conditions.push_back(another_condition_stub)
+	assert_true(dialogue_starter_step.is_enabled())
+
+
+func test_dialogue_starter_step_is_enabled_returns_false_if_any_enable_conditions_are_not_enabled() -> void:
+	stub(event_conditional_stub.can_trigger_condition.bind("", true, EventsManager.events_map)).to_return(true)
+	assert_true(dialogue_starter_step.is_enabled())
+	
+	var another_condition_stub: HFEventConditional = double(HFEventConditional).new()
+	stub(another_condition_stub.can_trigger_condition.bind("", true, EventsManager.events_map)).to_return(false)
+	dialogue_starter_step.enable_conditions.push_back(another_condition_stub)
+	assert_false(dialogue_starter_step.is_enabled())
