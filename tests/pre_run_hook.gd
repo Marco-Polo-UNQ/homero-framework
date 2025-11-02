28abd5fd84c8ea8fa262218f98fd6d698c90e9b5
R4nKF1v3 <nahumoscatelli@gmail.com>
2025-11-02 17:11:26 -0300

    chore: Added exceptions to coverage for untestable scripts
    * Added dialogue_editor_inspector_plugin.gd and dialogue_editor_inspector_widget.gd to Coverage exceptions given that they can't be instantiated or manipulated during Gut runtime contexts, only on Editor contexts.

--- tests/pre_run_hook.gd
+++ tests/pre_run_hook.gd
@@ -4,6 +4,11 @@ const exclude_paths = [
 	"res://addons/coverage/*",
 	"res://addons/gut/*",
 	"res://addons/homero_framework/plugin.gd",
+	# NOTE: Excluding EditorInspectorPlugin and EditorProperty because they
+	# can't be instanced manually in runtime, interfacing only within the Editor
+	# systems, ergo they can't be tested with GUT.
+	"res://addons/homero_framework/src/core/editor/dialogue/dialogue_editor_inspector_plugin.gd",
+	"res://addons/homero_framework/src/core/editor/dialogue/dialogue_editor_inspector_widget.gd",
 	# NOTE: Godot may crash if you try to instrument the script that's calling instrument_scripts()
 	"res://tests/*",
 	"res://contrib/*"
