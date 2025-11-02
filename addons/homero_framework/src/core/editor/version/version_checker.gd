ef2c4b1e23b0df2fcc2e3aad9c52c9f79f294c30
R4nKF1v3 <nahumoscatelli@gmail.com>
2025-11-02 17:13:50 -0300

    feat: Added version checker testing and documentation

--- addons/homero_framework/src/core/editor/version/version_checker.gd
+++ addons/homero_framework/src/core/editor/version/version_checker.gd
@@ -1,53 +1,62 @@
 @tool
 extends Node
+## Editor tool for checking the latest version of the plugin against GitHub releases.
 
-## Editor tool that automatically checks the latest version of the plugin
-## against the latest and notifies the user if there are any differences.
-
-const api_request_route: String = "https://api.github.com/repos/Marco-Polo-UNQ/homero-framework/releases/latest"
+## API route for fetching the latest release from GitHub.
+const API_REQUEST_URL: String = "https://api.github.com/repos/Marco-Polo-UNQ/homero-framework/releases/latest"
+## Preloaded scene for the popup warning dialog.
 const popup_warning_scene: PackedScene = preload(
 	"res://addons/homero_framework/src/core/editor/version/version_checker_popup.tscn"
 )
 
-var http_request: HTTPRequest
+## The HTTPRequest node used for API requests.
+var http_request
 
-
+# Called when the node is added to the scene tree.
 func _ready() -> void:
 	http_request = HTTPRequest.new()
 	add_child(http_request)
 
-
+## Requests a version check against the latest version from GitHub.
 func check_version(current_version: String) -> void:
 	if http_request.request_completed.is_connected(_on_http_request_completed):
 		http_request.request_completed.disconnect(_on_http_request_completed)
-	
 	http_request.request_completed.connect(_on_http_request_completed.bind(current_version))
-	var status: int = http_request.request(api_request_route)
-	if status != OK:
+	
+	var status: int = http_request.request(API_REQUEST_URL)
+	if status == OK:
+		HFLog.d("Version check request sent successfully, awaiting response.")
+	else:
 		HFLog.e("Error fetching version data %s" % status)
 
-
+# Handles the HTTP request completion and shows a popup if the version is outdated.
 func _on_http_request_completed(
 	result: int,
 	response_code: int,
 	_headers: PackedStringArray,
 	body: PackedByteArray,
 	current_version: String
-	) -> void:
+) -> void:
 	var json_parsed: JSON = JSON.new()
 	var status: int = json_parsed.parse(body.get_string_from_utf8())
-	if status == OK:
-		var data: Dictionary = json_parsed.data
-		if data.has("tag_name"):
-			var latest_version: String = data["tag_name"]
-			if latest_version != current_version:
-				HFLog.e("Version is different from latest, current is '%s' latest is '%s'. If you have an outdated version, please update the plugin to receive the latest features!" % [current_version, latest_version])
-				var popup: Window = popup_warning_scene.instantiate()
-				popup.enter(current_version, latest_version)
-				EditorInterface.popup_dialog(popup)
-			else:
-				HFLog.d("Up to date!")
-		else:
-			HFLog.e("Error fetching version data, the field 'name' doesn't exist!\nParsed data is %s" % data)
-	else:
+	if status != OK:
 		HFLog.e("Error parsing response for version data %s with body %s" % [status, body])
+		return
+
+	var data: Dictionary = json_parsed.data
+	if !data.has("tag_name"):
+		HFLog.e("Error fetching version data, the field 'tag_name' doesn't exist!\nParsed data is %s" % data)
+		return
+
+	var latest_version: String = data["tag_name"]
+	if latest_version == current_version:
+		HFLog.d("Up to date!")
+		return
+
+	HFLog.e("Version is different from latest, current is '%s' latest is '%s'. If you have an outdated version, please update the plugin to receive the latest features!" % [current_version, latest_version])
+	var popup: Window = popup_warning_scene.instantiate()
+	popup.enter(current_version, latest_version)
+	if Engine.is_editor_hint():
+		EditorInterface.popup_dialog(popup)
+	else:
+		popup.queue_free()
---
+++ tests/version/test_version_checker.gd
@@ -0,0 +1,200 @@
+extends GutTest
+
+const VERSION_CHECKER_SCENE_PATH: String = "res://addons/homero_framework/src/core/editor/version/version_checker.tscn"
+
+var version_checker: Node
+
+var http_request_mock
+
+
+class MockHTTPRequest:
+	extends Node
+	
+	signal request_completed
+	
+	func request(path: String) -> Error:
+		return 0
+
+
+func before_all() -> void:
+	set_double_strategy(DOUBLE_STRATEGY.INCLUDE_NATIVE)
+	register_inner_classes(get_script())
+
+
+func before_each() -> void:
+	version_checker = load(VERSION_CHECKER_SCENE_PATH).instantiate()
+	add_child_autoqfree(version_checker)
+	
+	# Mock the HTTPRequest node to avoid real HTTP requests during tests
+	var old_http_request: HTTPRequest = version_checker.http_request
+	version_checker.remove_child(old_http_request)
+	old_http_request.queue_free()
+	
+	http_request_mock = double(MockHTTPRequest).new()
+	version_checker.http_request = http_request_mock
+	version_checker.add_child(http_request_mock)
+
+
+func test_version_checker_node_exists() -> void:
+	assert_not_null(version_checker)
+	assert_true(version_checker is Node)
+
+
+func test_version_checker_has_http_request() -> void:
+	assert_eq(
+		version_checker.http_request,
+		http_request_mock
+	)
+	assert_true(
+		version_checker.is_ancestor_of(http_request_mock)
+	)
+	assert_eq(
+		version_checker.get_child_count(),
+		1
+	)
+
+
+func test_version_checker_can_check_version_with_positive_request_response() -> void:
+	stub(
+		http_request_mock.request.bind(
+			version_checker.API_REQUEST_URL
+		)
+	).to_return(OK)
+	assert_false(
+		http_request_mock.request_completed.is_connected(
+			version_checker._on_http_request_completed
+		)
+	)
+	version_checker.check_version("1.0.0")
+	assert_true(
+		http_request_mock.request_completed.is_connected(
+			version_checker._on_http_request_completed
+		)
+	)
+	assert_called(
+		http_request_mock,
+		"request",
+		[version_checker.API_REQUEST_URL]
+	)
+	assert_push_error_count(0)
+
+
+func test_version_checker_handles_http_request_with_negative_request_response() -> void:
+	stub(
+		http_request_mock.request.bind(
+			version_checker.API_REQUEST_URL
+		)
+	).to_return(ERR_CANT_CONNECT)
+	assert_false(
+		http_request_mock.request_completed.is_connected(
+			version_checker._on_http_request_completed
+		)
+	)
+	version_checker.check_version("1.0.0")
+	assert_true(
+		http_request_mock.request_completed.is_connected(
+			version_checker._on_http_request_completed
+		)
+	)
+	assert_called(
+		http_request_mock,
+		"request",
+		[version_checker.API_REQUEST_URL]
+	)
+	assert_push_error("Error fetching version data %s" % ERR_CANT_CONNECT)
+
+
+func test_version_checker_on_http_request_completed_with_valid_data() -> void:
+	stub(
+		http_request_mock.request.bind(
+			version_checker.API_REQUEST_URL
+		)
+	).to_return(OK)
+	version_checker.check_version("1.0.0")
+	
+	var mock_response_body: String = '{"tag_name": "1.0.0"}'
+	var mock_body_bytes: PackedByteArray = PackedByteArray()
+	for c in mock_response_body.to_utf8_buffer():
+		mock_body_bytes.append(c)
+	
+	http_request_mock.request_completed.emit(
+		OK,
+		200,
+		PackedStringArray(),
+		mock_body_bytes
+	)
+	assert_push_error_count(0)
+
+
+func test_version_checker_on_http_request_completed_with_outdated_version() -> void:
+	stub(
+		http_request_mock.request.bind(
+			version_checker.API_REQUEST_URL
+		)
+	).to_return(OK)
+	version_checker.check_version("1.0.0")
+	
+	var mock_response_body: String = '{"tag_name": "1.1.0"}'
+	var mock_body_bytes: PackedByteArray = PackedByteArray()
+	for c in mock_response_body.to_utf8_buffer():
+		mock_body_bytes.append(c)
+	
+	http_request_mock.request_completed.emit(
+		OK,
+		200,
+		PackedStringArray(),
+		mock_body_bytes
+	)
+	assert_push_error("Version is different from latest, current is '1.0.0' latest is '1.1.0'. If you have an outdated version, please update the plugin to receive the latest features!")
+	# Here you would check for expected behavior, such as logging or popup creation
+	# Since we cannot capture logs or UI that runs in the editor in this test, we assume
+	# the function would open a popup in the actual editor environment.
+	# This limits the total coverage of this test.
+
+
+func test_version_checker_on_http_request_completed_with_invalid_json() -> void:
+	stub(
+		http_request_mock.request.bind(
+			version_checker.API_REQUEST_URL
+		)
+	).to_return(OK)
+	version_checker.check_version("1.0.0")
+	
+	var mock_response_body: String = '{"invalid_json": "hi"}'
+	var mock_body_bytes: PackedByteArray = PackedByteArray()
+	for c in mock_response_body.to_utf8_buffer():
+		mock_body_bytes.append(c)
+	
+	http_request_mock.request_completed.emit(
+		OK,
+		200,
+		PackedStringArray(),
+		mock_body_bytes
+	)
+	assert_push_error(
+		"Error fetching version data, the field 'tag_name' doesn't exist!\nParsed data is %s" % {"invalid_json": "hi"}
+	)
+
+
+func test_version_checker_on_http_request_completed_with_parse_error() -> void:
+	stub(
+		http_request_mock.request.bind(
+			version_checker.API_REQUEST_URL
+		)
+	).to_return(OK)
+	version_checker.check_version("1.0.0")
+	
+	var mock_response_body: String = ""
+	var mock_body_bytes: PackedByteArray = PackedByteArray()
+	for c in mock_response_body.to_utf8_buffer():
+		mock_body_bytes.append(c)
+	
+	http_request_mock.request_completed.emit(
+		OK,
+		200,
+		PackedStringArray(),
+		mock_body_bytes
+	)
+	assert_push_error(
+		"Error parsing response for version data %s with body %s" % [ERR_PARSE_ERROR, mock_response_body]
+	)
