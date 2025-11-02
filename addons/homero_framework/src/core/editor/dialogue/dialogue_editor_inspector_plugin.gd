320118f5205e9540169edab464d2f5c12668d822
R4nKF1v3 <nahumoscatelli@gmail.com>
2025-11-02 17:20:48 -0300

    feat: Added dialogue editor inspector plugin and widget documentation

--- addons/homero_framework/src/core/editor/dialogue/dialogue_editor_inspector_plugin.gd
+++ addons/homero_framework/src/core/editor/dialogue/dialogue_editor_inspector_plugin.gd
@@ -1,16 +1,20 @@
 extends EditorInspectorPlugin
+## Inspector plugin for editing dialogue sequences in the editor.
 
-signal edit_called(dialogue_sequence: HFDialogueSequence)
+## Emitted when the edit button is pressed for a dialogue sequence.
+signal edit_called(dialogue_sequence: HFDialogueSpeaker)
 
-var widget_script: Script = preload(
+## The script for the custom property editor widget.
+const WIDGET_SCRIPT: Script = preload(
 	"res://addons/homero_framework/src/core/editor/dialogue/dialogue_editor_inspector_widget.gd"
 )
 
-
+# Determines if this plugin can handle the given object.
 func _can_handle(object) -> bool:
 	return object is HFDialogueSequence
 
 
+# Parses a property and adds a custom editor if needed.
 func _parse_property(
 	object: Object,
 	type: Variant.Type,
@@ -20,12 +24,12 @@ func _parse_property(
 	usage_flags: int,
 	wide: bool
 ) -> bool:
-	if name == "edit_dialogue":
-		var widget: EditorProperty = widget_script.new()
+	if name == "_edit_dialogue":
+		var widget: EditorProperty = WIDGET_SCRIPT.new()
 		add_property_editor(name, widget)
 		widget.edit_called.connect(_on_edit_called)
 	return true
 
-
+# Handles the edit_called signal from the widget.
 func _on_edit_called(dialogue_sequence: HFDialogueSequence) -> void:
 	edit_called.emit(dialogue_sequence)
--- addons/homero_framework/src/core/editor/dialogue/dialogue_editor_inspector_widget.gd
+++ addons/homero_framework/src/core/editor/dialogue/dialogue_editor_inspector_widget.gd
@@ -1,12 +1,15 @@
 @tool
 extends EditorProperty
+## Inspector property widget for editing dialogue sequences in the editor.
 
+## Emitted when the edit button is pressed for a dialogue sequence.
 signal edit_called(dialogue_sequence: HFDialogueSequence)
 
-# The main control for editing the property.
+## The main control for editing the property.
 var property_control: Button
 
 
+# Initializes the property widget and sets up the edit button.
 func _init() -> void:
 	property_control = Button.new()
 	add_child(property_control)
@@ -17,6 +20,6 @@ func _init() -> void:
 	read_only = true
 	deletable = false
 
-
+# Handles the edit button being pressed.
 func _on_edit_pressed() -> void:
 	edit_called.emit(get_edited_object())
