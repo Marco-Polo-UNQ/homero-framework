836917f77a914e3e594467b0a1a1b5b7a8cf0e7d
R4nKF1v3 <nahumoscatelli@gmail.com>
2025-11-03 02:07:23 -0300

    fix: Fixed post run hook error on coverage passing and some general printing settings

--- tests/post_run_hook.gd
+++ tests/post_run_hook.gd
@@ -9,7 +9,8 @@ func run():
 	if coverage_file:
 		coverage.save_coverage_file(coverage_file)
 	coverage.set_coverage_targets(COVERAGE_TARGET, FILE_TARGET)
-	Coverage.finalize(Coverage.Verbosity.FILENAMES)
+	Coverage.finalize(Coverage.Verbosity.PARTIAL_FILES)
+	#Coverage.finalize(Coverage.Verbosity.FILENAMES)
 	#Coverage.finalize(Coverage.Verbosity.FAILING_FILES)
 	var logger = gut.get_logger()
 	var coverage_passing = coverage.coverage_passing()
@@ -23,7 +24,6 @@ func run():
 		)
 		set_exit_code(2)
 	else:
-		gut.set_log_level(gut.LOG_LEVEL_ALL_ASSERTS)
 		logger.passed(
 			"Coverage target reached:\nActual %.1f%% of %.1f%% total, %.1f%% file coverage.\nLines covered %s/%s." %
 			[coverage_percent, COVERAGE_TARGET, FILE_TARGET, coverage_count, coverage_line_count]
